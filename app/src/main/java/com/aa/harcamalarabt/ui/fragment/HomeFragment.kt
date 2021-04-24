a931796e737093715b52bf48a3890c175cf4aaf6
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-04-24 16:51:58 +0300

    HomeFragment design completed

--- app/src/main/java/com/aa/harcamalarabt/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/aa/harcamalarabt/ui/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.aa.harcamalarabt.R
 import com.aa.harcamalarabt.databinding.FragmentHomeBinding
 
@@ -21,6 +22,41 @@ class HomeFragment : Fragment() {
         val view = binding.root
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonTl.setTextColor(ContextCompat.getColor(requireContext(), R.color.orange))
+
+        binding.buttonDolar.setOnClickListener {
+            binding.buttonDolar.setTextColor(ContextCompat.getColor(requireContext(), R.color.orange))
+            binding.buttonTl.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonSterlin.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonEuro.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+        }
+
+        binding.buttonSterlin.setOnClickListener {
+            binding.buttonSterlin.setTextColor(ContextCompat.getColor(requireContext(), R.color.orange))
+            binding.buttonTl.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonDolar.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonEuro.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+        }
+
+        binding.buttonEuro.setOnClickListener {
+            binding.buttonEuro.setTextColor(ContextCompat.getColor(requireContext(), R.color.orange))
+            binding.buttonTl.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonSterlin.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonDolar.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+        }
+
+        binding.buttonTl.setOnClickListener {
+            binding.buttonTl.setTextColor(ContextCompat.getColor(requireContext(), R.color.orange))
+            binding.buttonDolar.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonSterlin.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+            binding.buttonEuro.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray))
+        }
+
     }
 
 }
